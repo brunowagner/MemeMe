3b94f9e807f0b5959ec30a99f2de2e3a5ddacc9e
Bruno Wagner <brunowvmoraes@gmail.com>
2018-05-21 23:18:33 -0300

    Create Outlet to imageView and Actions to buttons

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func pickeAnPhoto(_ sender: Any) {
+    }
+    
+    @IBAction func pickerAnImage(_ sender: Any) {
+    }
+    
 }
 
