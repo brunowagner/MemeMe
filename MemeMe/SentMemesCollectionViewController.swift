706afb16b99370978426ec2225995ba11f0e7d33
Bruno Wagner <brunowvmoraes@gmail.com>
2018-06-11 18:51:42 -0300

    Add sent meme collection view controller

--- MemeMe/SentMemesCollectionViewController.swift
+++ MemeMe/SentMemesCollectionViewController.swift
@@ -10,10 +10,11 @@ import Foundation
 import UIKit
 class SentMemesCollectionViewController: UICollectionViewController {
     
-    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    var memes : [Meme]!
     
     override func viewDidLoad() {
-        
+        self.memes = self.appDelegate.memes
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
